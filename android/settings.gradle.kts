143c435c8a0887f5b83b7af98480a614f21df35e
NeonGD1667 <leanbinht@gmail.com>
2026-06-22 14:34:49 +0700

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,8 +19,6 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application")  apply false // remove ver
-    id("org.jetbrains.kotlin.android")  apply false
 }
 
 include(":app")
