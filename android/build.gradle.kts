882e7e7798b11a79c29fcac9ada3bcfbdedc9e74
NeonGD1667 <leanbinht@gmail.com>
2026-06-22 14:37:41 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,9 @@
 // android/build.gradle.kts
 plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android")  apply false
-    id("dev.flutter.flutter-gradle-plugin") apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
 allprojects {
