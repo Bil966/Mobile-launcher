d5e4ef2ceb387b4b7eddd37202ac93377b7a70b0
NeonGD1667 <leanbinht@gmail.com>
2026-06-22 14:57:44 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,6 @@
+
+    // android/build.gradle.kts
 plugins {
-    // Ép phiên bản tại đây để Flutter không tự ý "Upgrade" gây lỗi
     id("com.android.application") version "8.7.0" apply false
     id("com.android.library") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
