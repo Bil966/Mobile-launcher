45c2c33ff79ad878b0f3ba1fc5f0446daee2b714
NeonGD1667 <leanbinht@gmail.com>
2026-06-22 14:08:16 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects.
+
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,18 +14,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+// XÓA đoạn `project.evaluationDependsOn(":app")` đi vì nó gây lỗi build
+// Nếu bạn cần cấu hình thư mục build riêng, hãy để lại phần config buildDir
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
